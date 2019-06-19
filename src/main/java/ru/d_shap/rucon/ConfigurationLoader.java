f0da63eb02d1cca973d388039f0b3633000efec6
Dmitry Shapovalov <shenf@mail.ru>
2019-06-19 15:56:00 +0300

    Интерфейс для загрузчика конфигурации

---
+++ src/main/java/ru/d_shap/rucon/ConfigurationLoader.java
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Application run configuration helper library.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of  Application run configuration helper.
+//
+//  Application run configuration helper is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+//  Application run configuration helper is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.rucon;
+
+/**
+ * Configuration loader interface.
+ *
+ * @author Dmitry Shapovalov
+ */
+public interface ConfigurationLoader {
+
+    void load();
+
+    String getConfig(String name);
+
+}
