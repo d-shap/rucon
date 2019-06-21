cc928031380b88290a52ecffc5fa91afcceff7b1
Dmitry Shapovalov <shenf@mail.ru>
2019-06-21 17:30:53 +0300

    Добавил методы для получения свойств в виде примитивов

--- src/main/java/ru/d_shap/rucon/Configuration.java
+++ src/main/java/ru/d_shap/rucon/Configuration.java
@@ -66,4 +66,123 @@ public final class Configuration implements ConfigLoader {
         return null;
     }
 
+    /**
+     * Get the configuration property as String.
+     *
+     * @param name         the property name.
+     * @param defaultValue the default property value.
+     *
+     * @return the property value.
+     */
+    public String getPropertyAsString(final String name, final String defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return value;
+        }
+    }
+
+    /**
+     * Get the configuration property as boolean.
+     *
+     * @param name         the property name.
+     * @param defaultValue the default property value.
+     *
+     * @return the property value.
+     */
+    public boolean getPropertyAsBoolean(final String name, final boolean defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Boolean.parseBoolean(value);
+        }
+    }
+
+    /**
+     * Get the configuration property as int.
+     *
+     * @param name         the property name.
+     * @param defaultValue the default property value.
+     *
+     * @return the property value.
+     */
+    public int getPropertyAsInt(final String name, final int defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Integer.parseInt(value);
+        }
+    }
+
+    /**
+     * Get the configuration property as long.
+     *
+     * @param name         the property name.
+     * @param defaultValue the default property value.
+     *
+     * @return the property value.
+     */
+    public long getPropertyAsLong(final String name, final long defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Long.parseLong(value);
+        }
+    }
+
+    /**
+     * Get the configuration property as float.
+     *
+     * @param name         the property name.
+     * @param defaultValue the default property value.
+     *
+     * @return the property value.
+     */
+    public float getPropertyAsFloat(final String name, final float defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Float.parseFloat(value);
+        }
+    }
+
+    /**
+     * Get the configuration property as double.
+     *
+     * @param name         the property name.
+     * @param defaultValue the default property value.
+     *
+     * @return the property value.
+     */
+    public double getPropertyAsDouble(final String name, final double defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Double.parseDouble(value);
+        }
+    }
+
+    /**
+     * Get the configuration property as char.
+     *
+     * @param name         the property name.
+     * @param defaultValue the default property value.
+     *
+     * @return the property value.
+     */
+    public char getPropertyAsChar(final String name, final char defaultValue) {
+        String value = getProperty(name);
+        if (value == null || value.length() == 0) {
+            return defaultValue;
+        } else {
+            return value.charAt(0);
+        }
+    }
+
 }
