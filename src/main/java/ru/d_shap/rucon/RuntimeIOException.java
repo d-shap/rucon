0c945e3b887d659d7ac0bb2ccdd915496f3f6e87
Dmitry Shapovalov <shenf@mail.ru>
2019-06-21 16:54:31 +0300

    Класс ошибки

---
+++ src/main/java/ru/d_shap/rucon/RuntimeIOException.java
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// RuCon is the application configuration helper.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of RuCon.
+//
+// RuCon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RuCon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.rucon;
+
+import java.io.IOException;
+
+/**
+ * Runtime exception wrapper for the IO exception.
+ *
+ * @author Dmitry Shapovalov
+ */
+public class RuntimeIOException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create new object.
+     *
+     * @param exception IO exception.
+     */
+    public RuntimeIOException(final IOException exception) {
+        super(exception);
+    }
+
+}
