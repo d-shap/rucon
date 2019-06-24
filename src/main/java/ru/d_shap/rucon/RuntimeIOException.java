9815e3f3128a5fa9cb40c6afaed7fb5e92dd27a5
Dmitry Shapovalov <shenf@mail.ru>
2019-06-24 17:07:56 +0300

    Сделал final

--- src/main/java/ru/d_shap/rucon/RuntimeIOException.java
+++ src/main/java/ru/d_shap/rucon/RuntimeIOException.java
@@ -26,7 +26,7 @@ import java.io.IOException;
  *
  * @author Dmitry Shapovalov
  */
-public class RuntimeIOException extends RuntimeException {
+public final class RuntimeIOException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
