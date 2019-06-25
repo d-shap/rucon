9646f8aa558f8ec7e9b395f2e2b0d765e6c6f307
Dmitry Shapovalov <shenf@mail.ru>
2019-06-25 13:52:03 +0300

    Реализация конфигуратора

---
+++ src/main/java/ru/d_shap/rucon/ConfigurationLoader.java
@@ -0,0 +1,139 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// RuCon is the application configuration helper.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of RuCon.
+//
+// RuCon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RuCon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.rucon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Application configuration implementation.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ConfigurationLoader implements ConfigLoader, Configuration {
+
+    private final List<ConfigLoader> _configLoaders;
+
+    /**
+     * Create new object.
+     *
+     * @param configLoaders configuration loaders.
+     */
+    public ConfigurationLoader(final List<ConfigLoader> configLoaders) {
+        super();
+        _configLoaders = new ArrayList<>();
+        if (configLoaders != null) {
+            for (ConfigLoader configLoader : configLoaders) {
+                if (configLoader != null) {
+                    _configLoaders.add(configLoader);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void load() {
+        for (ConfigLoader configLoader : _configLoaders) {
+            configLoader.load();
+        }
+    }
+
+    @Override
+    public String getProperty(final String name) {
+        for (ConfigLoader configLoader : _configLoaders) {
+            String value = configLoader.getProperty(name);
+            if (value != null) {
+                return value;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getPropertyAsString(final String name, final String defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return value;
+        }
+    }
+
+    @Override
+    public boolean getPropertyAsBoolean(final String name, final boolean defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Boolean.parseBoolean(value);
+        }
+    }
+
+    @Override
+    public int getPropertyAsInt(final String name, final int defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Integer.parseInt(value);
+        }
+    }
+
+    @Override
+    public long getPropertyAsLong(final String name, final long defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Long.parseLong(value);
+        }
+    }
+
+    @Override
+    public float getPropertyAsFloat(final String name, final float defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Float.parseFloat(value);
+        }
+    }
+
+    @Override
+    public double getPropertyAsDouble(final String name, final double defaultValue) {
+        String value = getProperty(name);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return Double.parseDouble(value);
+        }
+    }
+
+    @Override
+    public char getPropertyAsChar(final String name, final char defaultValue) {
+        String value = getProperty(name);
+        if (value == null || value.length() == 0) {
+            return defaultValue;
+        } else {
+            return value.charAt(0);
+        }
+    }
+
+}
