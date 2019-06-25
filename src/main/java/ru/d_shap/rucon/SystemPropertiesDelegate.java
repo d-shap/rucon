0b253b72e062fd4807cd1a24dfd4a7706b3c56e5
Dmitry Shapovalov <shenf@mail.ru>
2019-06-25 22:41:32 +0300

    Делегат для системных свойств

---
+++ src/main/java/ru/d_shap/rucon/SystemPropertiesDelegate.java
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// RuCon is the application configuration helper.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of RuCon.
+//
+// RuCon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RuCon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.rucon;
+
+import java.util.Set;
+
+/**
+ * Configuration delegate for the system properties.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class SystemPropertiesDelegate extends BaseConfig implements ConfigDelegate {
+
+    /**
+     * Create new object.
+     *
+     * @param prefix            the prefix to add to the property name.
+     * @param suffix            the suffix to add to the property name.
+     * @param excludeProperties the properties to exclude.
+     */
+    public SystemPropertiesDelegate(final String prefix, final String suffix, final Set<String> excludeProperties) {
+        super(prefix, suffix, null, excludeProperties);
+    }
+
+    @Override
+    public String getProperty(final String name) {
+        if (isExcludeProperty(name)) {
+            return null;
+        } else {
+            String propertyName = getPropertyName(name);
+            return System.getProperty(propertyName);
+        }
+    }
+
+}
