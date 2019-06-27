78888ec5e7b254344f58dd3239f2398785142387
Dmitry Shapovalov <shenf@mail.ru>
2019-06-27 16:44:56 +0300

    Заглушка для тестов

---
+++ src/test/java/ru/d_shap/rucon/BaseConfigTest.java
@@ -0,0 +1,94 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// RuCon is the application configuration helper.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of RuCon.
+//
+// RuCon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RuCon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.rucon;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link BaseConfig}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class BaseConfigTest {
+
+    /**
+     * Test class constructor.
+     */
+    public BaseConfigTest() {
+        super();
+    }
+
+    /**
+     * {@link BaseConfig} class test.
+     */
+    @Test
+    public void fillSetTest() {
+        // TODO
+    }
+
+    /**
+     * {@link BaseConfig} class test.
+     */
+    @Test
+    public void fillMapTest() {
+        // TODO
+    }
+
+    /**
+     * {@link BaseConfig} class test.
+     */
+    @Test
+    public void fillPropertiesTest() {
+        // TODO
+    }
+
+    /**
+     * {@link BaseConfig} class test.
+     */
+    @Test
+    public void getPropertyNameTest() {
+        // TODO
+    }
+
+    /**
+     * {@link BaseConfig} class test.
+     */
+    @Test
+    public void isExcludePropertyTest() {
+        // TODO
+    }
+
+    /**
+     * {@link BaseConfig} class test.
+     */
+    @Test
+    public void getPropertyAliasTest() {
+        // TODO
+    }
+
+    /**
+     * {@link BaseConfig} class test.
+     */
+    @Test
+    public void replacePropertyAliasesTest() {
+        // TODO
+    }
+
+}
