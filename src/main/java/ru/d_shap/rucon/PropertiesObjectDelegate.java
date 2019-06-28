be0fbba0dbd27eb7993dcf2733e3ebff69dc4c57
Dmitry Shapovalov <shenf@mail.ru>
2019-06-28 11:48:18 +0300

    Делегат для получения свойств из объекта

---
+++ src/main/java/ru/d_shap/rucon/PropertiesObjectDelegate.java
@@ -0,0 +1,64 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// RuCon is the application configuration helper.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of RuCon.
+//
+// RuCon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RuCon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.rucon;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Configuration delegate for the properties.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class PropertiesObjectDelegate extends BaseConfig implements ConfigDelegate {
+
+    private final Map<Object, Object> _properties;
+
+    /**
+     * Create new object.
+     *
+     * @param properties        the properties object.
+     * @param excludeProperties the properties to exclude.
+     */
+    public PropertiesObjectDelegate(final Map<Object, Object> properties, final Set<String> excludeProperties) {
+        super(null, null, null, excludeProperties);
+        if (properties == null) {
+            _properties = new HashMap<>();
+        } else {
+            _properties = properties;
+        }
+    }
+
+    @Override
+    public String getProperty(final String name) {
+        if (isExcludeProperty(name)) {
+            return null;
+        } else {
+            Object value = _properties.get(name);
+            if (value == null) {
+                return null;
+            } else {
+                return value.toString();
+            }
+        }
+    }
+
+}
