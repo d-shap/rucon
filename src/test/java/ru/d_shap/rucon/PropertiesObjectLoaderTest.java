16e84547872ab19926b4fe5aa7defd6c6bc77c89
Dmitry Shapovalov <shenf@mail.ru>
2019-06-28 17:12:26 +0300

    Добавил дополнительные проверки в тест получения свойств

--- src/test/java/ru/d_shap/rucon/PropertiesObjectLoaderTest.java
+++ src/test/java/ru/d_shap/rucon/PropertiesObjectLoaderTest.java
@@ -79,6 +79,81 @@ public final class PropertiesObjectLoaderTest {
         Assertions.assertThat(loader02.getProperty("key")).isNull();
         Assertions.assertThat(loader02.getProperty("key1")).isNull();
         Assertions.assertThat(loader02.getProperty("key2")).isNull();
+
+        Map<Object, Object> properties03 = new HashMap<>();
+        properties03.put("key", "value");
+        Set<String> excludeProperties03 = null;
+        PropertiesObjectLoader loader03 = new PropertiesObjectLoader(properties03, excludeProperties03);
+        Assertions.assertThat(loader03.getProperty(null)).isNull();
+        Assertions.assertThat(loader03.getProperty("")).isNull();
+        Assertions.assertThat(loader03.getProperty(" ")).isNull();
+        Assertions.assertThat(loader03.getProperty("key")).isEqualTo("value");
+        Assertions.assertThat(loader03.getProperty("key1")).isNull();
+        Assertions.assertThat(loader03.getProperty("key2")).isNull();
+        loader03.load();
+        Assertions.assertThat(loader03.getProperty(null)).isNull();
+        Assertions.assertThat(loader03.getProperty("")).isNull();
+        Assertions.assertThat(loader03.getProperty(" ")).isNull();
+        Assertions.assertThat(loader03.getProperty("key")).isEqualTo("value");
+        Assertions.assertThat(loader03.getProperty("key1")).isNull();
+        Assertions.assertThat(loader03.getProperty("key2")).isNull();
+
+        Map<Object, Object> properties04 = new HashMap<>();
+        properties04.put("key1", "value1");
+        properties04.put("key2", "value2");
+        Set<String> excludeProperties04 = null;
+        PropertiesObjectLoader loader04 = new PropertiesObjectLoader(properties04, excludeProperties04);
+        Assertions.assertThat(loader04.getProperty(null)).isNull();
+        Assertions.assertThat(loader04.getProperty("")).isNull();
+        Assertions.assertThat(loader04.getProperty(" ")).isNull();
+        Assertions.assertThat(loader04.getProperty("key")).isNull();
+        Assertions.assertThat(loader04.getProperty("key1")).isEqualTo("value1");
+        Assertions.assertThat(loader04.getProperty("key2")).isEqualTo("value2");
+        loader04.load();
+        Assertions.assertThat(loader04.getProperty(null)).isNull();
+        Assertions.assertThat(loader04.getProperty("")).isNull();
+        Assertions.assertThat(loader04.getProperty(" ")).isNull();
+        Assertions.assertThat(loader04.getProperty("key")).isNull();
+        Assertions.assertThat(loader04.getProperty("key1")).isEqualTo("value1");
+        Assertions.assertThat(loader04.getProperty("key2")).isEqualTo("value2");
+
+        Map<Object, Object> properties05 = new HashMap<>();
+        properties05.put("key1", null);
+        properties05.put(null, "value2");
+        Set<String> excludeProperties05 = null;
+        PropertiesObjectLoader loader05 = new PropertiesObjectLoader(properties05, excludeProperties05);
+        Assertions.assertThat(loader05.getProperty(null)).isEqualTo("value2");
+        Assertions.assertThat(loader05.getProperty("")).isNull();
+        Assertions.assertThat(loader05.getProperty(" ")).isNull();
+        Assertions.assertThat(loader05.getProperty("key")).isNull();
+        Assertions.assertThat(loader05.getProperty("key1")).isNull();
+        Assertions.assertThat(loader05.getProperty("key2")).isNull();
+        loader05.load();
+        Assertions.assertThat(loader05.getProperty(null)).isEqualTo("value2");
+        Assertions.assertThat(loader05.getProperty("")).isNull();
+        Assertions.assertThat(loader05.getProperty(" ")).isNull();
+        Assertions.assertThat(loader05.getProperty("key")).isNull();
+        Assertions.assertThat(loader05.getProperty("key1")).isNull();
+        Assertions.assertThat(loader05.getProperty("key2")).isNull();
+
+        Map<Object, Object> properties06 = new HashMap<>();
+        properties06.put("key1", new StringBuilder("value1"));
+        properties06.put(new StringBuilder("key2"), "value2");
+        Set<String> excludeProperties06 = null;
+        PropertiesObjectLoader loader06 = new PropertiesObjectLoader(properties06, excludeProperties06);
+        Assertions.assertThat(loader06.getProperty(null)).isNull();
+        Assertions.assertThat(loader06.getProperty("")).isNull();
+        Assertions.assertThat(loader06.getProperty(" ")).isNull();
+        Assertions.assertThat(loader06.getProperty("key")).isNull();
+        Assertions.assertThat(loader06.getProperty("key1")).isEqualTo("value1");
+        Assertions.assertThat(loader06.getProperty("key2")).isEqualTo("value2");
+        loader06.load();
+        Assertions.assertThat(loader06.getProperty(null)).isNull();
+        Assertions.assertThat(loader06.getProperty("")).isNull();
+        Assertions.assertThat(loader06.getProperty(" ")).isNull();
+        Assertions.assertThat(loader06.getProperty("key")).isNull();
+        Assertions.assertThat(loader06.getProperty("key1")).isEqualTo("value1");
+        Assertions.assertThat(loader06.getProperty("key2")).isEqualTo("value2");
     }
 
 }
