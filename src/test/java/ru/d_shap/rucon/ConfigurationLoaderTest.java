595b42508270450406d727d5c1a4e8e6bf35c51a
Dmitry Shapovalov <shenf@mail.ru>
2019-07-01 16:55:53 +0300

    Заглушка для тестов

---
+++ src/test/java/ru/d_shap/rucon/ConfigurationLoaderTest.java
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// RuCon is the application configuration helper.
+// Copyright (C) 2019 Dmitry Shapovalov.
+//
+// This file is part of RuCon.
+//
+// RuCon is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// RuCon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.rucon;
+
+/**
+ * Tests for {@link ConfigurationLoader}.
+ *
+ * @author Dmitry Shapovalov
+ */
+public final class ConfigurationLoaderTest {
+
+    /**
+     * Test class constructor.
+     */
+    public ConfigurationLoaderTest() {
+        super();
+    }
+
+}
