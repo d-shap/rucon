0fac21b0a8c64ce7d354318a8149eb2e95848973
Dmitry Shapovalov <shenf@mail.ru>
2019-07-01 17:14:06 +0300

    Перенес тесты в другой пакет

--- src/test/java/ru/d_shap/rucon/loader/PropertiesObjectLoaderTest.java
+++ src/test/java/ru/d_shap/rucon/loader/PropertiesObjectLoaderTest.java
@@ -17,7 +17,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-package ru.d_shap.rucon;
+package ru.d_shap.rucon.loader;
 
 import java.util.HashMap;
 import java.util.HashSet;
