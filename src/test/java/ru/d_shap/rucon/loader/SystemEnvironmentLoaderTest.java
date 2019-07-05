b2889b6356860ec02755ad1a82632e11cbc94792
Dmitry Shapovalov <shenf@mail.ru>
2019-07-05 15:46:36 +0300

    Реализовал тесты на получение названий свойств

--- src/test/java/ru/d_shap/rucon/loader/SystemEnvironmentLoaderTest.java
+++ src/test/java/ru/d_shap/rucon/loader/SystemEnvironmentLoaderTest.java
@@ -19,7 +19,14 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.rucon.loader;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
 
 /**
  * Tests for {@link SystemEnvironmentLoader}.
@@ -40,7 +47,95 @@ public final class SystemEnvironmentLoaderTest {
      */
     @Test
     public void getNamesTest() {
-        // TODO
+        Map<String, String> aliases01 = null;
+        Set<String> excludeProperties01 = null;
+        SystemEnvironmentLoader loader01 = new SystemEnvironmentLoader(aliases01, excludeProperties01);
+        Assertions.assertThat(loader01.getNames()).containsExactly();
+        loader01.load();
+        Assertions.assertThat(loader01.getNames()).isNotEmpty();
+
+        Map<String, String> aliases02 = new HashMap<>();
+        Set<String> excludeProperties02 = null;
+        SystemEnvironmentLoader loader02 = new SystemEnvironmentLoader(aliases02, excludeProperties02);
+        Assertions.assertThat(loader02.getNames()).containsExactly();
+        loader02.load();
+        Assertions.assertThat(loader02.getNames()).isNotEmpty();
+
+        Map<String, String> aliases03 = new HashMap<>();
+        aliases03.put("path1", "PATH");
+        aliases03.put("path2", "ROOTPATH");
+        aliases03.put("path3", "Path");
+        Set<String> excludeProperties03 = null;
+        SystemEnvironmentLoader loader03 = new SystemEnvironmentLoader(aliases03, excludeProperties03);
+        Assertions.assertThat(loader03.getNames()).containsExactly();
+        loader03.load();
+        Assertions.assertThat(loader03.getNames()).containsAny("path1", "path2", "path3");
+
+        Map<String, String> aliases04 = new HashMap<>();
+        aliases04.put("path1", "PATH");
+        aliases04.put("path2", "ROOTPATH");
+        aliases04.put("path3", "Path");
+        Set<String> excludeProperties04 = new HashSet<>();
+        SystemEnvironmentLoader loader04 = new SystemEnvironmentLoader(aliases04, excludeProperties04);
+        Assertions.assertThat(loader04.getNames()).containsExactly();
+        loader04.load();
+        Assertions.assertThat(loader04.getNames()).containsAny("path1", "path2", "path3");
+
+        Map<String, String> aliases05 = new HashMap<>();
+        aliases05.put("path1", "PATH");
+        aliases05.put("path2", "ROOTPATH");
+        aliases05.put("path3", "Path");
+        Set<String> excludeProperties05 = new HashSet<>();
+        excludeProperties05.add("path1");
+        excludeProperties05.add("path2");
+        excludeProperties05.add("path3");
+        SystemEnvironmentLoader loader05 = new SystemEnvironmentLoader(aliases05, excludeProperties05);
+        Assertions.assertThat(loader05.getNames()).containsExactly();
+        loader05.load();
+        Assertions.assertThat(loader05.getNames()).isNotEmpty();
+        Assertions.assertThat(loader05.getNames()).containsNone("path1", "path2", "path3");
+
+        Map<String, String> aliases06 = new HashMap<>();
+        aliases06.put("path1", "PATH");
+        aliases06.put("path2", "ROOTPATH");
+        aliases06.put("path3", "Path");
+        Set<String> excludeProperties06 = null;
+        SystemEnvironmentLoader loader06 = new SystemEnvironmentLoader(aliases06, excludeProperties06);
+        Assertions.assertThat(loader06.getNames()).containsExactly();
+        loader06.load();
+        Assertions.assertThat(loader06.getNames()).containsAny("path1", "path2", "path3");
+        loader06.getNames().add("path");
+        Assertions.assertThat(loader06.getNames()).containsAny("path1", "path2", "path3");
+        Assertions.assertThat(loader06.getNames()).doesNotContain("path");
+        loader06.load();
+        Assertions.assertThat(loader06.getNames()).containsAny("path1", "path2", "path3");
+        Assertions.assertThat(loader06.getNames()).doesNotContain("path");
+
+        Map<String, String> aliases07 = new HashMap<>();
+        aliases07.put("path1", "PATH");
+        aliases07.put("path2", "ROOTPATH");
+        aliases07.put("path3", "Path");
+        Set<String> excludeProperties07 = new HashSet<>();
+        excludeProperties07.add("path1");
+        excludeProperties07.add("path2");
+        excludeProperties07.add("path3");
+        SystemEnvironmentLoader loader07 = new SystemEnvironmentLoader(aliases07, excludeProperties07);
+        Assertions.assertThat(loader07.getNames()).containsExactly();
+        loader07.load();
+        Assertions.assertThat(loader07.getNames()).isNotEmpty();
+        Assertions.assertThat(loader07.getNames()).containsNone("path1", "path2", "path3");
+        aliases07.clear();
+        Assertions.assertThat(loader07.getNames()).isNotEmpty();
+        Assertions.assertThat(loader07.getNames()).containsNone("path1", "path2", "path3");
+        loader07.load();
+        Assertions.assertThat(loader07.getNames()).isNotEmpty();
+        Assertions.assertThat(loader07.getNames()).containsNone("path1", "path2", "path3");
+        excludeProperties07.clear();
+        Assertions.assertThat(loader07.getNames()).isNotEmpty();
+        Assertions.assertThat(loader07.getNames()).containsNone("path1", "path2", "path3");
+        loader07.load();
+        Assertions.assertThat(loader07.getNames()).isNotEmpty();
+        Assertions.assertThat(loader07.getNames()).containsNone("path1", "path2", "path3");
     }
 
     /**
