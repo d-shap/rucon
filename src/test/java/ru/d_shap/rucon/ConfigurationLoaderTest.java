910b3d12cb85522c0ed7ef0409afcdd8f807f929
Dmitry Shapovalov <shenf@mail.ru>
2019-07-05 17:11:54 +0300

    Реализовал тесты на получение названий свойств

--- src/test/java/ru/d_shap/rucon/ConfigurationLoaderTest.java
+++ src/test/java/ru/d_shap/rucon/ConfigurationLoaderTest.java
@@ -19,7 +19,13 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 package ru.d_shap.rucon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import ru.d_shap.assertions.Assertions;
+import ru.d_shap.rucon.loader.PropertiesResourceLoader;
 
 /**
  * Tests for {@link ConfigurationLoader}.
@@ -40,7 +46,49 @@ public final class ConfigurationLoaderTest {
      */
     @Test
     public void getNamesTest() {
-        // TODO
+        List<ConfigDelegate> configDelegates01 = null;
+        ConfigurationLoader loader01 = new ConfigurationLoader(configDelegates01);
+        Assertions.assertThat(loader01.getNames()).containsExactly();
+        loader01.load();
+        Assertions.assertThat(loader01.getNames()).containsExactly();
+
+        List<ConfigDelegate> configDelegates02 = new ArrayList<>();
+        ConfigurationLoader loader02 = new ConfigurationLoader(configDelegates02);
+        Assertions.assertThat(loader02.getNames()).containsExactly();
+        loader02.load();
+        Assertions.assertThat(loader02.getNames()).containsExactly();
+
+        List<ConfigDelegate> configDelegates03 = new ArrayList<>();
+        configDelegates03.add(new PropertiesResourceLoader(getClass().getClassLoader(), "config1.properties", null));
+        ConfigurationLoader loader03 = new ConfigurationLoader(configDelegates03);
+        Assertions.assertThat(loader03.getNames()).containsExactly();
+        loader03.load();
+        Assertions.assertThat(loader03.getNames()).containsExactly("key1", "key2", "key3");
+
+        List<ConfigDelegate> configDelegates04 = new ArrayList<>();
+        configDelegates04.add(null);
+        configDelegates04.add(new PropertiesResourceLoader(getClass().getClassLoader(), "config1.properties", null));
+        configDelegates04.add(null);
+        ConfigurationLoader loader04 = new ConfigurationLoader(configDelegates04);
+        Assertions.assertThat(loader04.getNames()).containsExactly();
+        loader04.load();
+        Assertions.assertThat(loader04.getNames()).containsExactly("key1", "key2", "key3");
+
+        List<ConfigDelegate> configDelegates05 = new ArrayList<>();
+        configDelegates05.add(new PropertiesResourceLoader(getClass().getClassLoader(), "config1.properties", null));
+        configDelegates05.add(new PropertiesResourceLoader(getClass().getClassLoader(), "config2.properties", null));
+        ConfigurationLoader loader05 = new ConfigurationLoader(configDelegates05);
+        Assertions.assertThat(loader05.getNames()).containsExactly();
+        loader05.load();
+        Assertions.assertThat(loader05.getNames()).containsExactly("key1", "key2", "key3", "key4");
+
+        List<ConfigDelegate> configDelegates06 = new ArrayList<>();
+        configDelegates06.add(new PropertiesResourceLoader(getClass().getClassLoader(), "config2.properties", null));
+        configDelegates06.add(new PropertiesResourceLoader(getClass().getClassLoader(), "config1.properties", null));
+        ConfigurationLoader loader06 = new ConfigurationLoader(configDelegates06);
+        Assertions.assertThat(loader06.getNames()).containsExactly();
+        loader06.load();
+        Assertions.assertThat(loader06.getNames()).containsExactly("key1", "key2", "key3", "key4");
     }
 
     /**
